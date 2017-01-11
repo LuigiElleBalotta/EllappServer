20625185492cea37322a9c626b2da8ff7c67261d
Luigi Balotta <luigi.balotta@exelis.it>
2017-01-11 16:39:53 +0100

    Re-uploaded server. This can compile on Visual Studio for Mac on .Net Framework 4.6.1 (unfortunately there is no lib for Websocket on NetCore)

---
+++ Classes/CommandManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using MySql.Data.MySqlClient;
+
+namespace EllApp_server.Classes
+{
+    class CommandManager
+    {
+        static Config_Manager conf = new Config_Manager();
+        static MySqlConnection conn = new MySqlConnection("Server=" + conf.getValue("mysql_host") + ";Database=" + conf.getValue("mysql_db") + ";Uid=" + conf.getValue("mysql_user") + ";Pwd=" + conf.getValue("mysql_password") + ";");
+        public static void CreateAccount(string username, string password, string email)
+        {
+            byte[] passwordbyte = Encoding.ASCII.GetBytes(username + ":" + password);
+            var sha_pass = SHA1.Create();
+            byte[] bytehash = sha_pass.ComputeHash(passwordbyte);
+            var hashedpsw = Utility.HexStringFromBytes(bytehash);
+
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO accounts(username, password) VALUES(@username, @password, @email);", conn);
+            MySqlParameter passwordParameter = new MySqlParameter("@password", MySqlDbType.VarChar, 0);
+            MySqlParameter usernameParameter = new MySqlParameter("@username", MySqlDbType.VarChar, 0);
+            MySqlParameter emailParameter = new MySqlParameter("@email", MySqlDbType.VarChar, 0);
+            passwordParameter.Value = hashedpsw.ToUpper();
+            usernameParameter.Value = username.ToUpper();
+            emailParameter.Value = email.ToUpper();
+            cmd.Parameters.Add(usernameParameter);
+            cmd.Parameters.Add(passwordParameter);
+            cmd.Parameters.Add(emailParameter);
+            if(cmd.ExecuteNonQuery() >= 1)
+                Console.WriteLine("Account created.");
+            else
+                Console.WriteLine("Failure creating account.");
+        }
+    }
+}
---
+++ Classes/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EllApp_server.Classes
+{
+    class Utility
+    {
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
+        }
+    }
+}
